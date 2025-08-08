1439c7fc79629e60c419041c54fdb7ceb730fcf8
abhay333d <abhaydandge333@gmail.com>
2025-08-08 12:16:20 +0530

    done with the project

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -1,21 +1,123 @@
-import React from 'react'
-import './Quiz.css'
-const Quiz = () => {
-  return (
-    <div className='container' >
-          <h1>DL Quiz</h1>
-          <hr />
-          <h2>1. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit, sint!</h2>
-          <ul>
-              <li>dsagfasd</li>
-              <li>dsgfdgfd</li>
-              <li>fdsga</li>
-              <li>safewa</li>
-          </ul>
-          <button>Next</button>
-          <div className="index">1 of 15 questions</div>
-    </div>
-  )
-}
+import React, { useRef, useState } from "react";
+import "./Quiz.css";
+import { data } from "../../assets/data.js";
 
-export default Quiz
+const Quiz = () => {
+  let [index, setIndex] = useState(0);
+  let [question, setQuestion] = useState(data[index]);
+  let [lock, setLock] = useState(false);
+  let [score, setScore] = useState(0);
+  let [result, setResult] = useState(false);
+
+  let Option1 = useRef(null);
+  let Option2 = useRef(null);
+  let Option3 = useRef(null);
+  let Option4 = useRef(null);
+
+  let optionArr = [Option1, Option2, Option3, Option4];
+
+  const checkAns = (e, ans) => {
+    if (lock === false) {
+      if (question.ans === ans) {
+        e.target.classList.add("correct");
+        setLock(true);
+        setScore((prev) => prev + 1);
+      } else {
+        e.target.classList.add("wrong");
+        setLock(true);
+        optionArr[question.ans - 1].current.classList.add("correct");
+      }
+    }
+  };
+
+  const next = () => {
+    if (lock === true) {
+      if (index === data.length - 1) {
+        setResult(true);
+        return 0;
+      }
+      setIndex(++index);
+      setQuestion(data[index]);
+      setLock(false);
+
+      optionArr.map((option) => {
+        option.current.classList.remove("wrong", "correct");
+        return null;
+      });
+    }
+  };
+
+  const reset = () => {
+    setIndex(0);
+    setQuestion(data[0]);
+    setLock(false);
+    setScore(0);
+    setResult(false);
+  };
+
+  return (
+    <div className="container">
+      <h1>DL Quiz</h1>
+      <hr />
+      {result ? (
+        <></>
+      ) : (
+        <>
+          <h2>
+            {index + 1}. {question.question}
+          </h2>
+          <ul>
+            <li
+              ref={Option1}
+              onClick={(e) => {
+                checkAns(e, 1);
+              }}
+            >
+              {question.option2}
+            </li>
+            <li
+              ref={Option2}
+              onClick={(e) => {
+                checkAns(e, 2);
+              }}
+            >
+              {question.option1}
+            </li>
+            <li
+              ref={Option3}
+              onClick={(e) => {
+                checkAns(e, 3);
+              }}
+            >
+              {question.option3}
+            </li>
+            <li
+              ref={Option4}
+              onClick={(e) => {
+                checkAns(e, 4);
+              }}
+            >
+              {question.option4}
+            </li>
+          </ul>
+          <button onClick={next}>Next</button>
+          <div className="index">
+            {index + 1} of {data.length} questions
+          </div>
+        </>
+      )}
+      {result ? (
+        <>
+          <h2>
+            You scored {score} out of {data.length}{" "}
+          </h2>
+          <button onClick={reset}>Reset</button>
+        </>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+};
+
+export default Quiz;
